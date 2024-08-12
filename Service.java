b6c15884a4a38754a16dd3fc2a5f286b1bcaea48
joylyngongg <155286914+joylyngongg@users.noreply.github.com>
2024-08-12 15:49:58 -0400

    Add files via upload

---
+++ Service.java
@@ -0,0 +1,45 @@
+public class Service {
+    private String name;
+    private float price;
+    private int duration;
+    private String description;
+
+    public Service(String name, float price, int duration, String description) {
+        this.name = name;
+        this.price = price;
+        this.duration = duration;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
